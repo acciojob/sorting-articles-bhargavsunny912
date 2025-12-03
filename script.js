bf412fd02ddd55416ac701628b24e56d3084f8bd
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-12-03 14:39:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
         
         let s=[];
@@ -17,15 +16,15 @@ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'N
                 s.push(i);
             }
         }
-        s.sort((a,b)=>(a.trim()).localeCompare(b.trim()));
+        let sortedarr=s.sort((a,b)=>(a.trim()).localeCompare(b.trim()));
         
-       for(let j of s){
+       for(let j of sortedarr){
         for(let k of bands){
             if(k.includes(j)){
                 let li=document.createElement("li");
                 let hr=document.createElement("hr");
                 li.textContent=k;
-                document.getElementById("bands").append(li,hr);
+                document.getElementById("ul").append(li,hr);
             }
         }
        } 
